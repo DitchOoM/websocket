847882c345da88c038bbf16b3a039599eada9189
Rahul Behera <rbehera@gmail.com>
2024-02-16 16:19:44 -0800

    reduce timeout

--- karma.config.d/karma.conf.js
+++ karma.config.d/karma.conf.js
@@ -1,5 +1,5 @@
 config.client = config.client || {}
 config.client.mocha = config.client.mocha || {}
-config.client.mocha.timeout = 120000
-config.browserNoActivityTimeout = 120000
-config.browserDisconnectTimeout = 120000
+config.client.mocha.timeout = 40000
+config.browserNoActivityTimeout = 40000
+config.browserDisconnectTimeout = 40000
--- src/commonJvmTest/kotlin/RunTest.kt
+++ src/commonJvmTest/kotlin/RunTest.kt
@@ -5,7 +5,7 @@ import kotlin.time.Duration.Companion.seconds
 
 actual fun <T> block(body: suspend CoroutineScope.() -> T) {
     runBlocking {
-        withTimeout(180.seconds, body)
+        withTimeout(40.seconds, body)
     }
 }
 
