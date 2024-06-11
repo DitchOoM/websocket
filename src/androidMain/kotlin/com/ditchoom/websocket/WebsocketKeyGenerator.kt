0b0b3da55592bcf37fd5f52b1f6e02dbd7c5e3bc
Rahul Behera <rbehera@gmail.com>
2024-06-11 10:15:16 -0700

    use android base64

--- src/androidMain/kotlin/com/ditchoom/websocket/WebsocketKeyGenerator.kt
+++ src/androidMain/kotlin/com/ditchoom/websocket/WebsocketKeyGenerator.kt
@@ -6,9 +6,5 @@ import kotlin.random.nextInt
 
 actual fun generateWebSocketKey(): String {
     val randomBytes = ByteArray(16) { Random.nextInt(97..122).toByte() }
-    return try {
-        java.util.Base64.getEncoder().encodeToString(randomBytes)
-    } catch (t: Throwable) {
-        Base64.encodeToString(randomBytes, Base64.DEFAULT)
-    }
+    return Base64.encodeToString(randomBytes, Base64.DEFAULT)
 }
