4c748ff03776cc50bdca2361595ef98a79ec237d
Rahul Behera <rbehera@gmail.com>
2024-06-11 10:36:35 -0700

    use java base64 if it's a unit test

--- src/androidMain/kotlin/com/ditchoom/websocket/WebsocketKeyGenerator.kt
+++ src/androidMain/kotlin/com/ditchoom/websocket/WebsocketKeyGenerator.kt
@@ -1,10 +1,15 @@
 package com.ditchoom.websocket
 
+import android.os.Build
 import android.util.Base64
 import kotlin.random.Random
 import kotlin.random.nextInt
 
 actual fun generateWebSocketKey(): String {
     val randomBytes = ByteArray(16) { Random.nextInt(97..122).toByte() }
-    return Base64.encodeToString(randomBytes, Base64.DEFAULT)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O || Build.VERSION.SDK_INT == 0) {
+        java.util.Base64.getEncoder().encodeToString(randomBytes)
+    } else {
+        Base64.encodeToString(randomBytes, Base64.DEFAULT)
+    }
 }
