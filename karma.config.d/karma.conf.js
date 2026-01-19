99e2e4ea9eed49976e1de4e94fddd5c2044583c7
Rahul Behera <rbehera@gmail.com>
2026-01-19 17:39:25 -0600

    Increase browser test timeout to 2 minutes for large payload tests

--- karma.config.d/karma.conf.js
+++ karma.config.d/karma.conf.js
@@ -1,3 +1,5 @@
 config.client = config.client || {}
 config.client.mocha = config.client.mocha || {}
+// Increase timeout for large payload tests (case9 tests with 16MB payloads)
+config.client.mocha.timeout = 120000 // 2 minutes
 
