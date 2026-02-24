531a5916c4eece72f708e19027cbdc81983b8fb5
Rahul Behera <rbehera@gmail.com>
2026-02-23 20:12:08 -0600

    Update socket dependency from 2.1.2-SNAPSHOT to 2.1.2 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ project.version = getNextVersion(!isRunningOnGithub).toString()
 logger.lifecycle("Version: ${project.version}, isRunningOnGithub: $isRunningOnGithub, isMainBranchGithub: $isMainBranchGithub")
 
 repositories {
-    mavenLocal()
     mavenCentral()
     google()
     maven { setUrl("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers/") }
